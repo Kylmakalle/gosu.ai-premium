969d3ee0ae3139cb3615506b57e13b430dcc2d61
Sergey <iceman9831@gmail.com>
2018-11-16 14:33:39 +0300

    Update gosu_premium_script.user.js

--- gosu_premium_script.user.js
+++ gosu_premium_script.user.js
@@ -2,19 +2,25 @@
 // @name     Gosu.ai premium
 // @author   https://t.me/Kylmakalle
 // @include  https://gosu.ai/dota/analysis/match/*
-// @version  0.0.2
+// @version  0.0.3
 // @downloadURL https://raw.githubusercontent.com/Kylmakalle/gosu.ai-premium/master/gosu_premium_script.user.js
 // @updateURL https://raw.githubusercontent.com/Kylmakalle/gosu.ai-premium/master/gosu_premium_script.user.js
-// @run-at context-menu
+// @run-at document-idle
 // ==/UserScript==
 
 function getDivs(){
     return Array.from(document.getElementsByClassName('premium__gap ng-star-inserted'))
-           .concat(Array.from(document.getElementsByClassName('advice__gap ng-star-inserted')))
+        .concat(Array.from(document.getElementsByClassName('advice__gap ng-star-inserted')))
 }
 
-let premiumDivs = getDivs();
-premiumDivs.forEach(function(element) {
-    console.log(element);
-    element.remove();
-});
+function a () {
+    let premiumDivs = getDivs();
+    if (!premiumDivs.length){
+        setTimeout(a, 2000);
+    } else {
+        premiumDivs.forEach(function(element) {
+            element.remove();
+        });
+    }
+}
+a();
